961009b25f4b2d974b5fba650109d5d3e478a5ba
amalguduru <amalguduru@gmail.com>
2024-07-25 15:02:16 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+//             onclick="location.href='https://www.linkedin.com/in/amal-guduru/'"
